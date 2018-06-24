d58c27bb66c13ea1714dbfefe3df9f212a2476ca
Apurva <theapurvap@users.noreply.github.com>
2018-06-24 16:35:52 -0500

    Add files via upload

--- src/utilities/Config.js
+++ src/utilities/Config.js
@@ -21,5 +21,10 @@ export default {
             encrypt: ["encrypt"],
             decrypt: ["decrypt"]
         }
+    },
+    googleDrive: {
+        developerKey: "AIzaSyDWS9YPyy86r1OE1zeYfrJzeVZMgBTt1cs",
+        clientId: "705436738304-p6q37q3s4h2s36m16tot9gab8qb6moom.apps.googleusercontent.com",
+        scope: 'https://www.googleapis.com/auth/drive.readonly'
     }
 };
