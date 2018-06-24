3954315482ea0b20464ac04882161694c377f36c
Apurva <theapurvap@users.noreply.github.com>
2018-06-24 16:31:58 -0500

    Add basic auth logic for Google Drive

---
+++ Config.js
@@ -0,0 +1,30 @@
+export default {
+    maxFileSize: 1024 * 1024 * 128,
+    maxPasswordLength: 64,
+    encoding: "UTF-8",
+    fileExtension: "syncrypto",
+    key: {
+        type: "raw",
+        name: "PBKDF2",
+        extractable: false,
+        operations: ["deriveKey"],
+        saltSize: 32,
+        hash: "SHA-512",
+        iterations: 65536
+    },
+    algorithm: {
+        name: "AES-GCM",
+        keySize: 256,
+        ivSize: 32,
+        tagLength: 128,
+        options: {
+            encrypt: ["encrypt"],
+            decrypt: ["decrypt"]
+        }
+    },
+    googleDrive: {
+        developerKey: "AIzaSyDWS9YPyy86r1OE1zeYfrJzeVZMgBTt1cs",
+        clientId: "705436738304-p6q37q3s4h2s36m16tot9gab8qb6moom.apps.googleusercontent.com",
+        scope: 'https://www.googleapis.com/auth/drive.readonly'
+    }
+};
