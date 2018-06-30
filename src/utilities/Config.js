307f3369aa193bcf9c9bc8e06a16ecc3ec808eb7
Apurva <theapurvap@users.noreply.github.com>
2018-06-30 18:17:13 -0500

    Update Google API auth scope

--- src/utilities/Config.js
+++ src/utilities/Config.js
@@ -26,6 +26,6 @@ export default {
         developerKey: "AIzaSyDWS9YPyy86r1OE1zeYfrJzeVZMgBTt1cs",
         clientId: "705436738304-p6q37q3s4h2s36m16tot9gab8qb6moom.apps.googleusercontent.com",
         appId: 705436738304,
-        scope: "https://www.googleapis.com/auth/drive.readonly"
+        scope: "https://www.googleapis.com/auth/drive"
     }
 };
